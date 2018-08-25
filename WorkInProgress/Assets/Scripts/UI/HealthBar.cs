b3e55c3d8ba6bf672f977df05a6f41ff30a20051
SaitenHazard <ankondrift@gmail.com>
2018-08-25 16:03:49 +0600

    25/08/18 04:03PM

---
+++ WorkInProgress/Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    private float maxWidth;
+    private float currentWidth;
+
+    public int maxHP;
+    public int currentHP;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        maxWidth = rectTransform.rect.width;
+        currentWidth = maxWidth;
+    }
+
+    private void Update()
+    {
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+        float newWidth = (maxWidth / maxHP) * currentHP;
+
+        rectTransform.sizeDelta = 
+            new Vector2(newWidth, rectTransform.rect.height);
+    }
+}
