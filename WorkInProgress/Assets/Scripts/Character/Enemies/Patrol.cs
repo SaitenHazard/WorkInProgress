ffb3a3bf95f2edc1a86a2a8fbc9c8797612b3182
SaitenHazard <ankondrift@gmail.com>
2018-09-08 13:44:46 +0600

    09/08/18 01:44PM

---
+++ WorkInProgress/Assets/Scripts/Character/Enemies/Patrol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    private int numberOfPoints;
+
+    private void Awake()
+    {
+        numberOfPoints = transform.childCount;
+    }
+
+    private void OnEnable()
+    {
+        findClosestPatrol();
+    }
+
+    private void findClosestPatrol()
+    {
+        GameObject closesPetrol;
+        Vector2 minDistance;
+
+        for (int i = 0; i < numberOfPoints; i++)
+        {
+            
+        }
+    }
+
+    private void get
+}
