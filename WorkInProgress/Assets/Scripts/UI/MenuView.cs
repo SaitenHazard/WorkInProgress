575514604059847201cb4a4c2db57eaf95a2f29d
SaitenHazard <ankondrift@gmail.com>
2018-09-09 19:47:30 +0600

    09/09/18 07/49PM

---
+++ WorkInProgress/Assets/Scripts/UI/MenuView.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuView : MonoBehaviour
+{
+    public static MenuView Instance;
+    public RectTransform[] buttonTransform;
+    public GameObject[] menuObjects;
+    public Transform slected;
+
+    private int mainMenuIndex;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void OnEnable()
+    {
+        mainMenuIndex = 1;
+    }
+
+    private void Update()
+    {
+        UpdateView();
+    }
+
+    public void ChangeIndex(bool increment)
+    {
+        if (increment)
+        {
+            mainMenuIndex--;
+
+            if (mainMenuIndex == -1)
+                mainMenuIndex = 4;
+        }
+        else
+        {
+            mainMenuIndex++;
+
+            if (mainMenuIndex == 5)
+                mainMenuIndex = 0;
+        }
+    }
+
+    private void UpdateView()
+    {
+        slected.position = buttonTransform[mainMenuIndex].position;
+        menuObjects[mainMenuIndex].SetActive(true);
+
+        for(int i = 0; i < 4; i++)
+        {
+            if(mainMenuIndex != i)
+                menuObjects[i].SetActive(false);
+        }
+    }
+}
