5887fbb9ed9969385e4f03d0546a341bb53981c2
SaitenHazard <ankondrift@gmail.com>
2018-11-25 01:48:16 +0600

    11/25/18 01:48AM

---
+++ WorkInProgress/Assets/Scripts/General/InfiniteSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteSpawner : MonoBehaviour
+{
+    public GameObject [] enemies;
+    public float spawnTime;
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        int enemyIndex = Random.Range(0, enemies.Length - 1);
+        Debug.Log(enemyIndex);
+
+        GameObject instantiateObejct 
+            = Instantiate(enemies[enemyIndex], this.transform);
+
+        instantiateObejct.transform.position = this.transform.position;
+
+        Debug.Log(instantiateObejct.transform);
+        Debug.Log(instantiateObejct.transform.parent.name);
+
+        yield return new WaitForSeconds(spawnTime);
+
+        StartCoroutine(Spawn());
+    }
+}
